d1dbb2d939e175e39416ea4a37815cdeb37df3e9
Yurii <yurii.rudenko26@gmail.com>
2024-02-11 20:28:01 +0200

    added-jwt-check

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import userController from "../controllers/userController.js";
 const router = new Router();
+import authMiddleware from "../middleware/authMiddleware.js";
 
 router.post('/registration', userController.registration);
 router.post('/login', userController.login);
-router.get('/auth', userController.check);
+router.get('/auth', authMiddleware, userController.check);
 
 export default router
