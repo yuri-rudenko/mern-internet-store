a549e0b51786c04a6b17367fb5e3df2303258239
Yurii <yurii.rudenko26@gmail.com>
2024-02-12 15:22:24 +0200

    added-routing

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
+import { SHOP_ROUTE } from '../utils/consts';
 
 const AppRouter = () => {
-    return (
-        <div className='AppRouter'>
-            
-        
 
-        </div>
+    const isAuth = false;
+
+    return (
+        <Routes>
+            {isAuth && authRoutes.map(({ path, Component }) =>
+                <Route key={path} path={path} element={<Component />} />
+            )}
+            {publicRoutes.map(({ path, Component }) =>
+                <Route key={path} path={path} element={<Component />} />
+            )}
+            <Route path="*" element={<Navigate to={SHOP_ROUTE} replace />} />
+        </Routes>
     );
 }
 
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,8 @@
-import Admin from "./pages/Admin.js"
-import Auth from "./pages/Auth.js"
-import Basket from "./pages/Basket.js"
-import DevicePage from "./pages/DevicePage.js"
-import Shop from "./pages/Shop.js"
+import Admin from "./pages/Admin"
+import Auth from "./pages/Auth"
+import Basket from "./pages/Basket"
+import DevicePage from "./pages/DevicePage"
+import Shop from "./pages/Shop"
 import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "./utils/consts"
 
 export const authRoutes = [
